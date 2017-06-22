b68d25d4e965bfb2d22c98becfba21e80f1da5f5
Tair Assimov <tair@assimov.net>
2017-06-22 19:33:11 +0200

    Build a board with empty cells

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// Implement me
+function Game() {
+  for (var row = 0; row < 50; row++) {
+    for (var col = 0; col < 50; col++) {
+      $('.container').append($('<div>').addClass('cell'));
+    }
+  }
+}
+
+$(document).ready(function() {
+  var game = new Game();
+});
