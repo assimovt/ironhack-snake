23ac3a571bdacf028e8c6b9cc25be69ce0526964
Tair Assimov <tair@assimov.net>
2017-06-22 21:21:51 +0200

    Add right, up, and down move operations

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 function Snake() {
+  this.direction = 'left';
+
   this.body = [
     { row: 16, column: 20 },
     { row: 16, column: 21 },
@@ -7,6 +9,42 @@ function Snake() {
     { row: 16, column: 24 }
   ];
 }
+
+Snake.prototype.move = function() {
+  var head = this.body[0];
+
+  switch(this.direction) {
+    case 'left':
+      this.body.unshift({
+        row: head.row,
+        column: (head.column - 1 + 50) %  50
+      });
+      break;
+
+    // Your implementation
+    case 'right':
+      this.body.unshift({
+        row: head.row,
+        column: (head.column + 1) % 50
+      });
+      break;
+
+    case 'up':
+      this.body.unshift({
+        row: (head.row - 1 + 50) % 50,
+        column: head.column
+      });
+      break;
+
+    case 'down':
+      this.body.unshift({
+        row: (head.row + 1) % 50,
+        column: head.column
+      });
+      break;
+  }
+  this.body.pop();
+};
 
 function Game() {
   this.snake = new Snake();
@@ -33,6 +71,18 @@ Game.prototype.drawSnake = function() {
   });
 };
 
+Game.prototype.clearSnake = function() {
+  $('.snake').removeClass('snake');
+};
+
+Game.prototype.update = function() {
+  this.snake.move();
+  this.clearSnake();
+  this.drawSnake();
+};
+
+var game;
+
 $(document).ready(function() {
-  var game = new Game();
+  game = new Game();
 });
