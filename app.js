ee09ebc6449c38af1abbca84fdce62f45c4b5b62
Tair Assimov <tair@assimov.net>
2017-06-22 20:12:28 +0200

    Put a snake on the board

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
+function Snake() {
+  this.body = [
+    { row: 16, column: 20 },
+    { row: 16, column: 21 },
+    { row: 16, column: 22 },
+    { row: 16, column: 23 },
+    { row: 16, column: 24 }
+  ];
+}
+
 function Game() {
+  this.snake = new Snake();
+
   for (var row = 0; row < 50; row++) {
     for (var col = 0; col < 50; col++) {
-      $('.container').append($('<div>').addClass('cell'));
+      $('.container').append($('<div>')
+        .addClass('cell')
+        .attr('data-row', row)
+        .attr('data-col', col)
+      );
     }
   }
+
+  this.drawSnake();
 }
+
+Game.prototype.drawSnake = function() {
+  this.snake.body.forEach(function(position, index) {
+    var selector = '[data-row=' + position.row + ']' +
+                   '[data-col=' + position.column + ']';
+
+    $(selector).addClass('snake');
+  });
+};
 
 $(document).ready(function() {
   var game = new Game();
